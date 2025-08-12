0206985f87a3fff2637c64824b360b3fe333a231
Sander Rafael <sanderfernandez0812@gmail.com>
2025-08-12 11:57:10 -0400

    feature:creacion de repositorios y interfaces

--- APi-DGA-Infrastructure/Repositories/GenericRepository.cs
+++ APi-DGA-Infrastructure/Repositories/GenericRepository.cs
@@ -1,12 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Api_DGA.Application.Interfaces.Repositories;
+using APi_DGA_Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace APi_DGA_Infrastructure.Repositories
 {
-    internal class GenericRepository
+    public class GenericRepository<Entity> : IGenericRepository<Entity> where Entity : class
     {
+        private readonly InfrastructureContext _dbContext;
+
+        public GenericRepository(InfrastructureContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public virtual async Task<Entity> AddAsync(Entity entity)
+        {
+            await _dbContext.Set<Entity>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public virtual async Task<List<Entity>> AddRangeAsync(List<Entity> entities)
+        {
+            await _dbContext.Set<Entity>().AddRangeAsync(entities);
+            await _dbContext.SaveChangesAsync();
+            return entities;
+        }
+
+        public virtual async Task UpdateAsync(Entity entity, int id)
+        {
+            var entry = await _dbContext.Set<Entity>().FindAsync(id);
+            _dbContext.Entry(entry).CurrentValues.SetValues(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public virtual async Task DeleteAsync(Entity entity)
+        {
+            _dbContext.Set<Entity>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public virtual async Task<List<Entity>> GetAllListAsync()
+        {
+            return await _dbContext.Set<Entity>().ToListAsync();//Deferred execution
+        }
+
+        public virtual IQueryable<Entity> GetAllQuery()
+        {
+            return _dbContext.Set<Entity>().AsQueryable();
+        }
+
+        public virtual async Task<List<Entity>> GetAllListWithIncludeAsync(List<string> properties)
+        {
+            var query = _dbContext.Set<Entity>().AsQueryable();
+
+            foreach (string property in properties)
+            {
+                query = query.Include(property);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public virtual IQueryable<Entity> GetAllQueryWithInclude(List<string> properties)
+        {
+            var query = _dbContext.Set<Entity>().AsQueryable();
+
+            foreach (string property in properties)
+            {
+                query = query.Include(property);
+            }
+
+            return query.AsQueryable();
+        }
+
+        public virtual async Task<Entity> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<Entity>().FindAsync(id);
+        }
     }
 }
--- Api-DGA.Application/Interfaces/Services/IGenericService.cs
+++ Api-DGA.Application/Interfaces/Services/IGenericService.cs
@@ -2,8 +2,23 @@
 
 namespace Api_DGA.Application.Interfaces.Services
 {
-    public class IGenericService
+     public interface IGenericService<SaveViewModel, ViewModel, Model>
+          where SaveViewModel : class
+          where ViewModel : class
+          where Model : class
     {
+        Task Update(SaveViewModel vm, int id);
 
+        Task<SaveViewModel> Add(SaveViewModel vm);
+
+        Task<List<SaveViewModel>> AddRange(List<SaveViewModel> vm);
+
+        Task Delete(int id);
+
+        Task<SaveViewModel> GetByIdSaveViewModel(int id);
+
+        Task<List<ViewModel>> GetAllListViewModel();
+
+        IQueryable<ViewModel> GetAllQueryViewModel();
     }
 }
